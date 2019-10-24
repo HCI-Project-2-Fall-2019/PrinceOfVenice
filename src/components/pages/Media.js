d7d025732b9e505007a0a9d11d3d755e5c1f8ae8
Wilsen Kosasih <wilsenkosasih@Wilsens-MacBook-Pro.local>
2019-10-23 19:28:06 -0700

    added ingredients gallery

--- src/components/pages/Media.js
+++ src/components/pages/Media.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import {AppBar, Tabs, Tab} from '@material-ui/core';
+import {Tabs, Tab} from '@material-ui/core';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Food from './media/Food';
+import Ingredient from './media/Ingredient';
 
 class Media extends Component {
     constructor (props) {
@@ -25,52 +26,56 @@ class Media extends Component {
         const {value} = this.state;
         return (  
             <div style={boxStyle}>
-                <div>
+                <div style={{textAlign:"left"}}>
                     <a 
                         href="https://www.facebook.com/PrinceOfVeniceFoodTruck/" 
                         target="_blank"
                         rel="noopener noreferrer">
-                            <FacebookIcon style={{ fontSize: 100, color:"#3b5998" }}/>
+                            <FacebookIcon style={{ fontSize: 85, color:"#3b5998" }}/>
                     </a>
                     <a href="https://twitter.com/povfoodtruck" 
                         target="_blank"
                         rel="noopener noreferrer">
-                            <TwitterIcon style={{ fontSize: 100, color:"#1B95E0" }}/>
+                            <TwitterIcon style={{ fontSize: 85, color:"#1B95E0" }}/>
                     </a>
                     <a 
                         href="https://www.instagram.com/princeofvenicefoodtruck/" 
                         target="_blank"
                         rel="noopener noreferrer">
-                            <InstagramIcon style={{ fontSize: 100, color:"#383838" }}/>
+                            <InstagramIcon style={{ fontSize: 85, color:"#383838" }}/>
                     </a>
                 </div>
                 <div>
-                    <Tabs
-                        orientation="vertical"
-                        variant="scrollable"
-                        scrollButtons="on"
-                        indicatorColor="primary"
-                        value={value}
-                        onChange={this.handleChange}
-                        aria-label="Vertical tabs example"
-                        //className={classes.tabs}
-                        style={{backgroundColor:"lightblue", height: 402, width: "25%", float:"left"}}
-                    >
-                        <Tab label="Gallery One" component={Link} to="/Media/GalleryOne"/>
-                        <Tab label="Gallery Two" component={Link} to="/Media/GalleryTwo"/>
-                        <Tab label="Foods" component={Link} to="/Media/GalleryThree"/>
-                        <Tab label="Ingredients" component={Link} to="/Media/GalleryFour"/>
-                        <Tab label="Truck" component={Link} to="/Media/GalleryFive"/>
-                        <Tab label="Cooking" component={Link} to="/Media/GallerySix"/>
-                    </Tabs>
-                    <Switch style={{width: "50%"}}>
-                        <Route path="/Media/GalleryOne" component={FacebookIcon} />
-                        <Route path="/Media/GalleryTwo" component={TwitterIcon} />
-                        <Route path="/Media/GalleryThree" component={Food} />
-                        <Route path="/Media/GalleryFour" component={TwitterIcon} />
-                        <Route path="/Media/GalleryFive" component={TwitterIcon} />
-                        <Route path="/Media/GallerySix" component={TwitterIcon} />
-                    </Switch>
+                    <Router>
+                        <Tabs
+                            orientation="vertical"
+                            variant="scrollable"
+                            scrollButtons="on"
+                            indicatorColor="secondary"
+                            value={value}
+                            onChange={this.handleChange}
+                            aria-label="Vertical tabs example"
+                            //className={classes.tabs}
+                            style={{backgroundColor:"lightgrey", height: 402, width: "20%", float:"left", 
+                                borderRadius: "0px 0px 0px 5px"}}
+                        >
+                            <Tab label="Gallery One" component={Link} to="/Media/GalleryOne"/>
+                            <Tab label="Gallery Two" component={Link} to="/Media/GalleryTwo"/>
+                            <Tab label="Foods" component={Link} to="/Media/GalleryThree"/>
+                            <Tab label="Ingredients" component={Link} to="/Media/GalleryFour"/>
+                            <Tab label="Truck" component={Link} to="/Media/GalleryFive"/>
+                            <Tab label="Cooking" component={Link} to="/Media/GallerySix"/>
+                        </Tabs>
+                        <Switch style={{width: "50%"}}>
+                            <Route path="/Media/GalleryOne" component={FacebookIcon} />
+                            <Route path="/Media/GalleryTwo" component={TwitterIcon} />
+                            <Route path="/Media/GalleryThree" component={Food} />
+                            <Route path="/Media/GalleryFour" component={Ingredient} />
+                            <Route path="/Media/GalleryFive" component={TwitterIcon} />
+                            <Route path="/Media/GallerySix" component={TwitterIcon} />
+                            <Route path="/Media" component={Food} />
+                        </Switch>
+                    </Router>
                 </div>
             </div>       
         );
@@ -79,10 +84,10 @@ class Media extends Component {
 
 const boxStyle = {
     maxWidth: '100%',
-    paddingTop: 10,
-    paddingBottom: 20,
+    //paddingTop: 10,
+    //paddingBottom: 20,
     backgroundColor: 'rgba(255, 255, 255,1)',
-    borderRadius: 10,
+    borderRadius: 5,
     margin: "auto",
 };
 
--- src/components/pages/media/Ingredient.js
+++ src/components/pages/media/Ingredient.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import pic1 from "./foodimages/0.jpg"
-import pic2 from "./foodimages/1.jpg"
-import pic3 from "./foodimages/2.jpg"
-import pic4 from "./foodimages/3.jpg"
-import pic5 from "./foodimages/4.jpg"
-import pic6 from "./foodimages/5.jpg"
-import pic7 from "./foodimages/6.jpg"
-import pic8 from "./foodimages/7.jpg"
-import pic9 from "./foodimages/8.jpg"
-import pic10 from "./foodimages/9.jpg"
-import pic11 from "./foodimages/10.jpg"
-import pic12 from "./foodimages/12.jpg"
-import pic13 from "./foodimages/13.jpg"
-import pic14 from "./foodimages/14.jpg"
+import pic1 from "./ingredientfolder/0.jpg"
+import pic2 from "./ingredientfolder/1.jpg"
+import pic3 from "./ingredientfolder/2.jpg"
+import pic4 from "./ingredientfolder/3.jpg"
+import pic5 from "./ingredientfolder/4.jpg"
+import pic6 from "./ingredientfolder/5.jpg"
+import pic7 from "./ingredientfolder/6.jpg"
+import pic8 from "./ingredientfolder/7.jpg"
+import pic9 from "./ingredientfolder/8.jpg"
+import pic10 from "./ingredientfolder/9.jpg"
+import pic11 from "./ingredientfolder/10.jpg"
+import pic12 from "./ingredientfolder/11.jpg"
+import pic13 from "./ingredientfolder/12.jpg"
 
 const tileData = [
     {   
         img: pic1,
         title: 'Image',
         author: 'author',
-        cols: 1,
+        cols: 2,
     },
     {   
         img: pic2,
@@ -95,14 +94,8 @@ const tileData = [
         author: 'author',
         cols: 1,
     },
-    {   
-        img: pic14,
-        title: 'Image',
-        author: 'author',
-        cols: 1,
-    },
 ]
-class Food extends Component {
+class Ingredient extends Component {
     render() {
         return (  
                 <GridList cellHeight={250} cols={4} style={{width: "79%", height: 400, border: "1px gray solid "}}>
@@ -116,4 +109,4 @@ class Food extends Component {
     }
 }
 
-export default Food;
+export default Ingredient;
